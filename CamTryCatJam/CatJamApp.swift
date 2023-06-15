9b9fa8a293d168332bd255ec10bd3a8fe7b36342
Veronique FARAG <veronique.farag@ecole-hexagone.com>
2023-06-15 12:03:03 +0200

    initial commit

---
+++ CamTryCatJam/CatJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatJamApp.swift
+//  CatJam
+//
+//  Created by Véronique FARAG on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CatJam
+
+Test commit
